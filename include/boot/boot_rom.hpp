3b3b49f6c0f4e4c7905ea69c5a8697984519277c
yongjin CHO <165399892+jjyj2302@users.noreply.github.com>
2025-08-01 18:41:28 +0900

    Delete include directory
    chore : 해당 디렉토리에 관련 없는 파일이라 삭제

--- include/boot/boot_rom.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-class Chip8_32;
-
-class BootROM {
-public:
-    static void load_into_memory(Chip8_32& chip8);
-};
--- include/boot/boot_rom_data.hpp
+++
@@ -1,7 +0,0 @@
-// boot_rom_data.hpp - 자동 크기 조정 (더 안전한 방법)
-#pragma once
-#include <cstdint>
-#include <cstddef>
-
-extern const uint32_t BOOT_ROM[];  // 크기 제거 - 자동으로 맞춤
-extern const size_t BOOT_ROM_SIZE;  // 크기 상수 추가
